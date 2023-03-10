38e6ae1f429a1cdf8dc277810f564e06957fe37b
takeshivm <vargasmurillotakeshi@gmail.com>
2023-03-10 15:04:25 -0500

    Register works, but controller can't give message back

--- src/main/java/com/cursospring/curso/models/Usuario.java
+++ src/main/java/com/cursospring/curso/models/Usuario.java
@@ -1,14 +1,13 @@
 package com.cursospring.curso.models;
 
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.Id;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
+
 @Entity
 @Table(name = "usuarios")
 public class Usuario {
 
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "id")
     private Long id;
     @Column(name = "nombre")
--- src/main/resources/static/js/register.js
+++ src/main/resources/static/js/register.js
@@ -5,16 +5,37 @@ $(document).ready(function() {
 
 async function registrarUsuario(){
 
-    data
+    let data = {};
+    data.nombre = document.getElementById('txtFirstName').value;
+    data.apellido = document.getElementById('txtLastName').value;
+    data.email = document.getElementById('txtEmail').value;
+    data.password = document.getElementById('txtPassword').value;
+    let repeatPassword = document.getElementById('txtRepeatPassword').value;
+
+    if(repeatPassword != data.password){
+        alert('La contraseña no coincide');
+        return;
+    }
+    console.log(data);
     const request = await fetch('api/usuarios',{
         method: 'POST',
         headers: {
             'Accept': 'application/json',
             'Content-Type': 'application/json'
         },
-        body: JSON.stringify(data);
+        body: JSON.stringify(data)
     });
-    const usuarios = await request.json();
-    document.querySelector('#usuarios tbody').outerHTML = listadoHtml;
-
+    //const usuarios = await request.json();
+    const response = await request.text();
+    let usuarios;
+    if (response.trim() === '') {
+        console.log("La respuesta del servidor está vacía");
+        return;
+    }
+    try {
+      usuarios = JSON.parse(response);
+    } catch (e) {
+      console.log("Error al leer la respuesta del servidor:", e);
+      return;
+    }
 };
